28735cc8887f2865f6a617dfcb553df8ffd65f1d
Higinio01 <101358454+Higinio01@users.noreply.github.com>
2023-06-04 13:35:21 -0400

    Add files via upload

---
+++ PracticaJDBC/src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+import org.example.controladores.CrudTradicionalControlador;
+import org.example.controladores.PlantillasControlador;
+import org.example.servicios.BootStrapServices;
+import org.example.servicios.DataBaseServices;
+
+import java.sql.SQLException;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        Javalin app = Javalin.create(config -> {
+            //configurando los documentos estaticos.
+            config.staticFiles.add(staticFileConfig -> {
+                staticFileConfig.hostedPath = "/";
+                staticFileConfig.directory = "/publico";
+                staticFileConfig.location = Location.CLASSPATH;
+                staticFileConfig.precompress = false;
+                staticFileConfig.aliasCheck = null;
+            });
+        });
+        app.start(8000);
+        BootStrapServices.startDb();
+        DataBaseServices.getInstancia().testConexion();
+        BootStrapServices.crearTablas();
+        new CrudTradicionalControlador(app).aplicarRutas();
+        new PlantillasControlador(app).aplicarRutas();
+        BootStrapServices.stopDb();
+    }
+
+}
